57f40dbd1a96f5f9406b23fad4065162a7d0824b
Ritam Majumder <ritmmajumder0@gmail.com>
2023-10-09 14:01:23 +0530

    firebase integrated

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAt7vxNRji2f89kHW59YJ2WVkqFeEx7rXs",
+    authDomain: "apjc-b2247.firebaseapp.com",
+    projectId: "apjc-b2247",
+    storageBucket: "apjc-b2247.appspot.com",
+    messagingSenderId: "944539529506",
+    appId: "1:944539529506:web:5728e841fdcee6419f757b",
+    measurementId: "G-1JJ4Q54PJB"
+  };
